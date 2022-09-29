a5d256e20f986db7fafc174abd7d837449480ce4
Nisreen <Nisreen.abuzidan@gmail.com>
2022-09-29 10:49:14 +0000

    Remove Import

--- file_test.py
+++ file_test.py
@@ -1,21 +1,20 @@
-import pytest
 
 def test_calc_addition():
   # Function test the output of 2+4
-    output = 2+4
-    assert output == 6
+  output = 2+4
+  assert output == 6
 
 def test_calc_substraction():
   # Function test the output of 2-4
-    output = 2-5
-    assert output == -3
+  output = 2-5
+  assert output == -3
 
 def test_calc_multiply():
   # Function test the output of 2*4
-    output = 2*4
-    assert output == 8
+  output = 2*4
+  assert output == 8
 
 def test_coucou():
   # Function test if the output return 'hello'
-    output='hello'
-    assert output == 'hello'
+  output='hello'
+  assert output == 'hello'
